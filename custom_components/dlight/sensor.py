2720c04eba63c24af56faf6b877caaf40ce62504
David Kernan <dave@paddez.com>
2026-06-14 23:12:24 +0100

    fix(sensor): remove unused UnitOfTemperature import

--- custom_components/dlight/sensor.py
+++ custom_components/dlight/sensor.py
@@ -10,7 +10,7 @@ from homeassistant.components.sensor import (
     SensorStateClass,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import PERCENTAGE, UnitOfTemperature
+from homeassistant.const import PERCENTAGE
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import DeviceInfo, EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
